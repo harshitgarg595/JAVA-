0b0fd755d827749cb047ac6b4bd5c734d953ced8
Harshit Garg <63019578+harshitgarg595@users.noreply.github.com>
2021-01-26 20:52:59 +0530

    Reverse printing of numbers

---
+++ Decrement Loop.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i=45;
+        for (i =45; i>=0; i--) {
+            System.out.println(i);
+        }
+    }
+}
