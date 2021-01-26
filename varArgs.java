32a91b10f695d2ff20c67029b489accd739c696b
Harshit Garg <63019578+harshitgarg595@users.noreply.github.com>
2021-01-26 21:20:34 +0530

    Introduction to varArgs

---
+++ varArgs.java
@@ -0,0 +1,37 @@
+//when we can give as mant arguments to the funcation 
+package com.company;
+import java.util.Scanner;
+public class Main {
+    static int sum(int...values)
+            //int values[];
+    {  int sum=0;
+        for (int element : values) {
+           sum += element;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(" addation of numbers is "+ sum(4));
+
+    }
+}
+
+
+
+// when one more arguments are requried to execution 
+package com.company;
+import java.util.Scanner;
+public class Main {
+    static int sum(int x,int...values)
+            //int values[];
+    {  int sum=x;
+        for (int element : values) {
+           sum += element;
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println(" addation of numbers is "+ sum(4));
+
+    }
+}
